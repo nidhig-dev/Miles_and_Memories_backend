2325eb958a5ea3f29d5e1fc07207569f75333ab6
nidhig-dev <goyal.nidhi@gmail.com>
2025-10-10 16:55:30 -0400

    added login functionality

--- routes/userRoutes.mjs
+++ routes/userRoutes.mjs
@@ -11,9 +11,9 @@ import User from "../models/userSchema.mjs";
 const router=express.Router();
 dotenv.config();
 
-router.route("/")
+router.route("/register")
 //signup/register user route
-    //@route:/api/user
+    //@route:/api/user/register
     //@desc: Registers a new user
     //@access:public
 .post([
@@ -29,13 +29,8 @@ router.route("/")
     try{
         //get user registration data from req.body
         const{userName,email,password}=req.body;
-        
-        //check for req.body object
-        if(!userName||!email||!password){
-            res.status(400).json({errors:[{msg:"All fields are required!"}]})
-        }
-
-        let user=User.findOne({email});
+                
+        let user= await User.findOne({email});
         //throw error if user is found
         if(user){
             return res.json(400).json({errors:[{msg:"User Exists.Please log in."}]})
@@ -84,9 +79,63 @@ router.route("/")
 
 })
 //login route
-
-//get user info route
-//@route:/api/user/:id
+//Login user route
+//@route:/api/user/login
 //@desc: get user info by id of a registered user or logged in user
 //@access:public
+
+router.route("/login")
+    .post([check("email", "Please include an email").not().isEmpty(),
+        check("password", "Please include a password").not().isEmpty(),
+    ], async (req,res)=>{
+        const errors=validationResult(req);
+        //check if there are errors
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        //get email and password from req.body
+        try {
+            const { email, password } = req.body;
+            //find a user based on email
+            let user = await User.findOne({ email });
+            //if no user is found-throw error
+            if (!user) {
+                return res
+                    .status(400)
+                    .json({ errors: [{ msg: "Invalid Credentials" }] });
+            }
+            //if user is found, match the password user entered with the database
+            const isMatch = await bcrypt.compare(password, user.password);
+            //throw error if match is not found
+            if (!isMatch) {
+                return res
+                    .status(400)
+                    .json({ errors: [{ msg: "Invalid Credentials" }] });
+            }
+            //create a payload with object id
+            const payload = {
+                user: {
+                    id: user._id,
+                },
+            };
+            //create a token with payload,secret and options
+            jwt.sign(
+                payload,
+                process.env.jwtSecret,
+                { expiresIn: "6h" },
+                (err, token) => {
+                    if (err) throw err;
+
+                    res.status(201).json({ token });
+                }
+            );
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).json({ errors: [{ msg: "Server Error" }] });
+        }
+})
+//get user info route
+//@route:/api/user/
+//@desc: get user info by id of a registered user or logged in user
+//@access:protected
 export default router;
